830807d9d95c87fdf559d4e23742035f6442d1c0
Topbit Du <topbit.du@gmail.com>
2017-01-30 19:09:59 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/standard/models_rspec.rb
@@ -0,0 +1,2 @@
+require 'rspec/models/unidom/standard/standard_spec'             unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Standard::Standard'
+require 'rspec/models/unidom/standard/standard_associating_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Standard::StandardAssociating'
