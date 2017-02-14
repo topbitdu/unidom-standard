4753d1e732a762af848ad40852b6e48783157583
Topbit Du <topbit.du@gmail.com>
2017-02-14 10:03:47 +0800

    1, Improve the Standard spec for the validations on the #name attribute, & the #number attribute.

--- lib/rspec/models/unidom/standard/standard_spec.rb
+++ lib/rspec/models/unidom/standard/standard_spec.rb
@@ -17,7 +17,32 @@ describe Unidom::Standard::Standard, type: :model do
       obsoleted_on: Date.current+20.years
     }
 
+    name_max_length   = described_class.columns_hash['name'].limit
+    number_max_length = described_class.columns_hash['number'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+    it_behaves_like 'validates', model_attributes, :name,
+      {             } => 0,
+      { name: nil   } => 2,
+      { name: ''    } => 2,
+      { name: 'A'   } => 1,
+      { name: 'AA'  } => 0,
+      { name: 'AAA' } => 0,
+      { name: 'A'*(name_max_length-1) } => 0,
+      { name: 'A'*name_max_length     } => 0,
+      { name: 'A'*(name_max_length+1) } => 1
+
+    it_behaves_like 'validates', model_attributes, :number,
+      {               } => 0,
+      { number: nil   } => 2,
+      { number: ''    } => 2,
+      { number: 'A'   } => 1,
+      { number: 'AA'  } => 0,
+      { number: 'AAA' } => 0,
+      { number: 'A'*(name_max_length-1) } => 0,
+      { number: 'A'*name_max_length     } => 0,
+      { number: 'A'*(name_max_length+1) } => 1
 
   end
 
