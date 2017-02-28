816d953cc59f4b1de5df4927f12d3a445dad2b73
Topbit Du <topbit.du@gmail.com>
2017-02-28 18:29:31 +0800

    1, Improve the Standard Associating spec for the source_is scope, the target_is scope.

--- lib/rspec/models/unidom/standard/standard_associating_spec.rb
+++ lib/rspec/models/unidom/standard/standard_associating_spec.rb
@@ -16,6 +16,18 @@ describe Unidom::Standard::StandardAssociating, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    it_behaves_like 'scope', :source_is, [
+      { attributes_collection: [ model_attributes                                     ], count_diff: 1, args: [ model_attributes[:source_id]                                     ] },
+      { attributes_collection: [ model_attributes                                     ], count_diff: 1, args: [ Unidom::Standard::Standard.new(id: model_attributes[:source_id]) ] },
+      { attributes_collection: [ model_attributes.merge(source_id: SecureRandom.uuid) ], count_diff: 0, args: [ model_attributes[:source_id]                                     ] },
+      { attributes_collection: [ model_attributes.merge(source_id: SecureRandom.uuid) ], count_diff: 0, args: [ Unidom::Standard::Standard.new(id: model_attributes[:source_id]) ] } ]
+
+    it_behaves_like 'scope', :target_is, [
+      { attributes_collection: [ model_attributes                                     ], count_diff: 1, args: [ model_attributes[:target_id]                                     ] },
+      { attributes_collection: [ model_attributes                                     ], count_diff: 1, args: [ Unidom::Standard::Standard.new(id: model_attributes[:target_id]) ] },
+      { attributes_collection: [ model_attributes.merge(target_id: SecureRandom.uuid) ], count_diff: 0, args: [ model_attributes[:target_id]                                     ] },
+      { attributes_collection: [ model_attributes.merge(target_id: SecureRandom.uuid) ], count_diff: 0, args: [ Unidom::Standard::Standard.new(id: model_attributes[:target_id]) ] } ]
+
   end
 
 end
