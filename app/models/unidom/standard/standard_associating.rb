972ced310776b729b57be6dcfff8bff58463d10f
Topbit Du <topbit.du@gmail.com>
2017-03-05 20:07:23 +0800

    1, Improve the Standard Associating model for the source_is scope, & the target_is scope.

--- app/models/unidom/standard/standard_associating.rb
+++ app/models/unidom/standard/standard_associating.rb
@@ -8,11 +8,11 @@ class Unidom::Standard::StandardAssociating < Unidom::Standard::ApplicationRecor
 
   include Unidom::Common::Concerns::ModelExtension
 
-  scope :source_is, ->(source) { where source_id: (source.respond_to?(:id) ? source.id : source) }
-  scope :target_is, ->(target) { where target_id: (target.respond_to?(:id) ? target.id : target) }
-
   belongs_to :source, class_name: 'Unidom::Standard::Standard', foreign_key: :source_id
   belongs_to :target, class_name: 'Unidom::Standard::Standard', foreign_key: :target_id
+
+  scope :source_is, ->(source) { where source: source }
+  scope :target_is, ->(target) { where target: target }
 
   def self.associate!(source, with: target, due_to: 'REVS', at: Time.now)
     create! source_id: to_id(source), target_id: to_id(with), association_code: due_to, opened_at: at
