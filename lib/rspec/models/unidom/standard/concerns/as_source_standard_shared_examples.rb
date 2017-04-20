d45fd2ccce9b133d82675cca42ebe1f036f69b7f
Topbit Du <topbit.du@gmail.com>
2017-04-20 17:11:16 +0800

    1, Improve the As Source Standard shared examples for the argument validations of the #associate! method, & the #associate? method.

--- lib/rspec/models/unidom/standard/concerns/as_source_standard_shared_examples.rb
+++ lib/rspec/models/unidom/standard/concerns/as_source_standard_shared_examples.rb
@@ -12,6 +12,18 @@ shared_examples 'Unidom::Standard::Concerns::AsSourceStandard' do |model_attribu
 
     it_behaves_like 'has_many', model_attributes, :target_associatings, Unidom::Standard::StandardAssociating, [ target_associating_1_attributes, target_associating_2_attributes ]
 
+    model = described_class.create! model_attributes
+
+    target_attributes = {
+      name:     'Target Standard Name',
+      #number:   'GB/T 9999-1845.A',
+      ics_code: '01-02-03'
+    }
+    target = Unidom::Standard::Standard.number_is('GB/T 9999-1845.A').first_or_create! target_attributes
+
+    it_behaves_like 'assert_present!', model, :associate!, [ target, due_to: 'REVS', at: Time.now ], [ { 0 => :target }, :due_to, :at ]
+    it_behaves_like 'assert_present!', model, :associate?, [ target, due_to: 'REVS', at: Time.now ], [ { 0 => :target }, :due_to, :at ]
+
   end
 
 end
